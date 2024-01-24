2b749e7a7359922dbba295d0158bb0b922ac74c3
deggerfire <70328071+deggerfire@users.noreply.github.com>
2024-01-24 11:16:05 -0600

    insert cool git message

---
+++ R/myquad.R
@@ -0,0 +1,14 @@
+#' A quadratic function
+#'
+#' @param x a numeric vector
+#'
+#' @return a numeric vector
+#' @export
+#'
+#' @examples
+#' myquad(x = 1:10)
+myquad <- function(x){
+  x^2 - 5*x +6
+}
+# code -> insert roxgen skeleton
+# build -> install
