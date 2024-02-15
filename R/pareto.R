b723c2be1212b79fe6b943cfcf75b5de3406bb6f
deggerfire <70328071+deggerfire@users.noreply.github.com>
2024-02-14 21:27:35 -0600

    added more methods

---
+++ R/pareto.R
@@ -0,0 +1,28 @@
+#' Pareto graphing funcation
+#'
+#' @param x the data in vector form
+#' @param mn graph title
+#' @param ... extra stuff
+#'
+#' @export
+#'
+#' @examples
+#'freq=c(15,8,63,20)
+#'RL=c("None","Both","LegsO","WheelsO")
+#'l=rep(RL,freq)
+#'pareto(l)
+pareto <- function(x, mn = "Pareto barplot", ...) {
+  x.tab = table(x)
+  xx.tab = sort(x.tab, decreasing = TRUE, index.return = FALSE)
+  cumsum(as.vector(xx.tab)) -> cs
+  length(x.tab) -> lenx
+  bp <- barplot(xx.tab, ylim = c(0,max(cs)),las = 2)
+  lb <- seq(0,cs[lenx], l = 11)
+  axis(side = 4, at = lb, labels = paste(seq(0, 100, length = 11), "%", sep = ""), las = 1, line = -1, col = "Blue", col.axis = "Red")
+  for(i in 1:(lenx-1)){
+    segments(bp[i], cs[i], bp[i+1], cs[i+1], col = i, lwd = 2)
+  }
+  title(main = mn, ...)
+}
+
+
